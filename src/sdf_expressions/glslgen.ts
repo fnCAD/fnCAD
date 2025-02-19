0afebeb9dc31b3861679d36d2cffdcc13593b28d
FeepingCreature <feepingcreature@gmail.com>
2025-02-19 17:55:45 +0100

    Oops: fix operator precedence when generating GLSL.

--- src/sdf_expressions/glslgen.ts
+++ src/sdf_expressions/glslgen.ts
@@ -137,7 +137,7 @@ export class GLSLContext {
     this.useVar(this.currentPoint);
     const self = this;
     return this.withPoint(
-      this.generator.save('vec3', () => `${self.currentPoint} - vec3(${dx}, ${dy}, ${dz})`)
+      this.generator.save('vec3', () => `(${self.currentPoint} - vec3(${dx}, ${dy}, ${dz}))`)
     );
   }
 
